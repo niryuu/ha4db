4614c8fdbe8966587447f8fb6bf2e75fd13028a8
Itsuro Tajima <tajima@georepublic.de>
2021-02-14 07:56:52 +0000

    (refs #64) fixing pointcloud css wip

--- app/javascript/packs/pointcloud/lib/initView.ts
+++ app/javascript/packs/pointcloud/lib/initView.ts
@@ -2,8 +2,15 @@ import * as THREE from 'three';
 import { Ha4dbControls } from './Ha4dbControls';
 import { TilesRenderer } from '3d-tiles-renderer';
 
+function onLoadModel(scene) {
+	scene.traverse( c => {
+		c.material.size = 4;
+	});
+}
+
 // ... initialize three scene ...
 function initView() {
+	const div = document.querySelector('#pointcloud');
 	// get data
 	const target = 'pointcloud'
 	const target_obj = document.getElementById(target)
@@ -22,26 +29,22 @@ function initView() {
 	const anotambientLight = new THREE.AmbientLight(0xffffff, 1.0);
 	annotationScene.add(anotambientLight);
 	// make camera
-	const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 2000);
+	const camera = new THREE.PerspectiveCamera(70, div.clientWidth / div.clientHeight, 0.1, 2000);
 	camera.position.x = parseFloat(initialX);
 	camera.position.y = parseFloat(initialY);
 	camera.position.z = parseFloat(initialZ);
 	// make renderer and attach to dom
 	const renderer = new THREE.WebGLRenderer();
-	const div = document.querySelector('#pointcloud');
-	div.appendChild(renderer.domElement);
-	renderer.domElement.width = div.clientWidth * window.devicePixelRatio;
-	renderer.domElement.height = div.clientHeight * window.devicePixelRatio;
-	renderer.domElement.style.width = div.clientWidth + 'px';
-	renderer.domElement.style.height = div.clientHeight + 'px';
-	const dom = renderer.domElement;
+	renderer.setSize(div.clientWidth, div.clientHeight);
 	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(dom.clientWidth, dom.clientHeight);
+	div.appendChild(renderer.domElement);
+	const dom = renderer.domElement;
 	renderer.autoClear = false;
 	// init 3D tiles renderer
 	const tilesRenderer = new TilesRenderer(path);
 	tilesRenderer.setCamera(camera);
 	tilesRenderer.setResolutionFromRenderer(camera, renderer);
+	tilesRenderer.onLoadModel = onLoadModel;
 	scene.add(tilesRenderer.group);
 	const clock = new THREE.Clock();
 	// attach control
